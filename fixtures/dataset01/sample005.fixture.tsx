17d80f81250e5bcd6dec1324cc4fa54b6606c396
seveibar <seveibar@gmail.com>
2026-02-27 13:08:53 -0800

    add sample005

---
+++ fixtures/dataset01/sample005.fixture.tsx
@@ -0,0 +1,16 @@
+import { GenericSolverDebugger } from "@tscircuit/solver-utils/react"
+import { defaultParams } from "../../lib/default-params"
+import { HighDensitySolverA01 } from "../../lib/HighDensitySolverA01/HighDensitySolverA01"
+import sample005 from "../../tests/dataset01/sample005/sample005.json"
+
+export default () => (
+  <GenericSolverDebugger
+    createSolver={() => {
+      const solver = new HighDensitySolverA01({
+        ...defaultParams,
+        nodeWithPortPoints: sample005.nodeWithPortPoints,
+      })
+      return solver
+    }}
+  />
+)
